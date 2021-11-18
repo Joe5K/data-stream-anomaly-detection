cc295e4c5348dee089d1a5c586feac5d3e232cf8
Joe5K <jozo.krkosk4@gmail.com>
2021-11-18 19:48:38 +0100

    Added some training data, mean and variance counting, working on anomaly detection problem

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # data-stream-anomaly-detection
-Semestralny projekt FEI VŠB
+## Semestralny projekt FEI VŠB
 
 Cílem práce je popsat metody pro analýzy datových toků sae zamřením na změny v rámci delších časových období. Na zvolených datech a datech metody porovnat a ověřit jejich účinnosti. Očekávaným výstupem projektu je:
 
@@ -8,11 +8,11 @@ Cílem práce je popsat metody pro analýzy datových toků sae zamřením na zm
 3. Prezentace na cca 10 minut
 4. Data pro experimenty
 
-Požadavky na zdrojový kód:
+### Požadavky na zdrojový kód:
 1. Dobře strukturovaný zdrojový kód dle standardu jazyka s členění do tříd (kde to dává smysl),
 2. Zdrojový kód okomentovaný v anglickém jazyce (Doxygen syntaxe nebo jiná strojové zpracovatelná)
 
-Obsah textové části
+### Obsah textové části
 1. Seznámení s problematikou
 2. State of the art
 3. Podrobný popis vybraných metod
@@ -21,9 +21,8 @@ Obsah textové části
 
 ### Motivacia 🥵
 
-#### Poznámky:
+#### Stipendium:
+5000 Kč
 
 Výše stipendia je závislá na dosažených výsledcích. Uvedená částka je za semestr.
 
-#### Stipendium:
-5000 Kč
--- run.py
+++ run.py
@@ -1,2 +1,65 @@
-if __name__ == "__main__":
-    print("Hello World")
+import csv
+import math
+from math import sqrt, pi, exp
+from decimal import Decimal
+
+
+def matrix_to_float(matrix: list) -> list:
+    matrix_out = []
+    for i in matrix:
+        matrix_out.append([float(j) for j in i])
+    return matrix_out
+
+
+def transpose_matrix(matrix: list) -> list:
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+
+def count_mean(data: list) -> float:
+    mean = 0
+    for i in data:
+        mean += i
+    return mean/len(data)
+
+
+def count_variance(data: list, mean: float) -> float:
+    variance = 0
+    for i in data:
+        variance += (i - mean)**2
+    return variance/len(data)
+
+
+def count_mean_and_variance_of_matrix(data):
+    transposed_data = transpose_matrix([i for i in data if i[-1] != 1])
+    means = []
+    variances = []
+    for i in transposed_data[:-1]:
+        if transposed_data[-1] == 1:
+            continue
+        mean = count_mean(i)
+        means.append(mean)
+        variance = count_variance(i, mean)
+        variances.append(variance)
+
+    return means, variances
+
+
+def get_anomalies(data: list, mean: list, variance: list):
+    epsylon = 0
+    for row in data:
+        row_anomaly = Decimal(1)
+        for i, (x, u, o) in enumerate(zip(row, mean, variance)):
+            if o == 0 or u == x:
+                continue
+            row_anomaly *= Decimal((1/(sqrt(2*pi)*o)) * exp(-((x-u)**2)/(2*(o**2))))  # tu je cosi dojebane
+        print(row_anomaly)
+
+
+with open('data/Participants_Data_WH18/Train.csv', newline='') as csvfile:
+    train = list(csv.reader(csvfile))
+    means, variances = count_mean_and_variance_of_matrix(matrix_to_float(train[1:]))
+
+with open('data/Participants_Data_WH18/Test.csv', newline='') as csvfile:
+    test = list(csv.reader(csvfile))
+    get_anomalies(matrix_to_float(test[1:]), means, variances)
+
