4c0516fa9e15f84c8eec4fd363d31aa225322323
Joe5K <jozo.krkosk4@gmail.com>
2022-05-10 11:32:13 +0200

    optimizing

--- eddm/eddm.py
+++ eddm/eddm.py
@@ -9,16 +9,11 @@ from eddm.eddmstats import RunningEDDMStats, EDDMStats
 from config import SKIP_FIRST_LINE
 
 
-class EDDMData:
-    def __init__(self):
+class EDDM:
+    def __init__(self, train_instances=100, error_threshold=0.75):
         self.running_mean = RunningVectorStatistics()
         self.misprediction_distance_stats = RunningEDDMStats()
         self.maximum_misprediction_distance = EDDMStats()
-
-
-class EDDM:
-    def __init__(self, train_instances=100, error_threshold=0.75):
-        self.data = EDDMData()
         self.train_instances = train_instances
         self.error_threshold = error_threshold
 
@@ -30,7 +25,7 @@ class EDDM:
             for line in reader.readlines():
                 counter += 1
                 new_vector = Vector.generate_vector(line)
-                if self.data.running_mean.count < self.train_instances:
+                if self.running_mean.count < self.train_instances:
                     self.train(new_vector)
                     last_error = counter
                     continue
@@ -55,33 +50,33 @@ class EDDM:
                 self.train(new_vector)
 
     def process_misprediction(self, number_of_processed_without_error):
-        self.data.misprediction_distance_stats.push(number_of_processed_without_error)
+        self.misprediction_distance_stats.push(number_of_processed_without_error)
 
-        if self.data.misprediction_distance_stats > self.data.maximum_misprediction_distance:
-            self.data.maximum_misprediction_distance.cache_stats(self.data.misprediction_distance_stats)
+        if self.misprediction_distance_stats > self.maximum_misprediction_distance:
+            self.maximum_misprediction_distance.cache_stats(self.misprediction_distance_stats)
 
     def is_error(self) -> bool:
-        if not self.data.maximum_misprediction_distance:
+        if not self.maximum_misprediction_distance:
             return False
-        result = self.data.misprediction_distance_stats / self.data.maximum_misprediction_distance
+        result = self.misprediction_distance_stats / self.maximum_misprediction_distance
         return result < self.error_threshold
 
     def reset(self):
-        self.data.misprediction_distance_stats.reset()
-        self.data.maximum_misprediction_distance.reset()
-        self.data.running_mean.reset()
+        self.misprediction_distance_stats.reset()
+        self.maximum_misprediction_distance.reset()
+        self.running_mean.reset()
 
     def train(self, new_vector: Vector):
-        self.data.running_mean.push(new_vector)
+        self.running_mean.push(new_vector)
 
     def predict_class(self, new_vector):
         found_class = None
         highest_probability = 0
 
-        for cls in self.data.running_mean.classes:
-            mean = self.data.running_mean.mean[cls]
-            variance = self.data.running_mean.variance[cls]
-            cls_count_division = self.data.running_mean.get_cls_count(cls)/self.data.running_mean.count
+        for cls in self.running_mean.classes:
+            mean = self.running_mean.mean[cls]
+            variance = self.running_mean.variance[cls]
+            cls_count_division = self.running_mean.get_cls_count(cls)/self.running_mean.count
             probability = naive_bayers_distance(new_vector, mean, variance, cls_count_division)
             if probability > highest_probability:
                 highest_probability = probability
